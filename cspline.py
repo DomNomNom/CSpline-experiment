c8d75b8a29a74b1e357bf2c19450a6e6162ac92e
Dominik Schmid <dominikschmid93@gmail.com>
2019-01-11 13:58:47 +1100

    Add CSpline and visualization thereof

---
+++ cspline.py
@@ -0,0 +1,80 @@
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import itertools
+
+def Vec2(x, y):
+    return np.array([x,y], dtype=float)
+def Vec3(x, y, z):
+    return np.array([x,y,z], dtype=float)
+
+def lerp(a,b,t):
+    """ Linear interpolation between from @a to @b as @t goes between 0 an 1. """
+    return (1-t)*a + t*b
+
+class CSpline:
+    """
+    A cubic hermite spline where defined by control points which define
+       "center"s - positions that the curve passes through as well as
+       "handle"s - positions relative to a center which defines the tangent in that direction.
+    """
+    def __init__(self, control_points: np.array):
+        '''
+        control_points has this structure:
+        [
+            center_0,
+            handle_before_center_0,
+            handle_after_center_0,
+            center_1,
+            handle_before_center_1,
+            handle_after_center_1,
+            center_2,
+            handle_before_center_2,
+            handle_after_center_2,
+            ...
+        ]
+        '''
+        self.control_points = control_points
+
+    def i_center_before(self, t:float):
+        return int(t) * 3
+    def i_center_after(self, t:float):
+        return self.i_center_before(t) + 3
+    def i_handle_before(self, t:float):
+        return self.i_center_before(t) + 2
+    def i_handle_after(self, t:float):
+        return self.i_center_before(t) + 3+1
+
+    def get_pos(self, t: float):
+        # Handle things outside the normal range by extrapolating.
+        max_interp_t = len(self.control_points)//3 - 1
+        if t <= 0: # extrapolate at start
+            A = self.control_points[0]
+            B = self.control_points[1]
+            return lerp(A, B, -t)
+        elif t >= max_interp_t: # Extrapolate at end
+            A = self.control_points[-3]
+            B = self.control_points[-1]
+            return lerp(A, B, t - max_interp_t)  # linear extrapolation
+
+
+        # Variable names according to this:
+        # https://thumbs.gfycat.com/HarmoniousHarshAdmiralbutterfly-size_restricted.gif
+        A = self.control_points[self.i_center_before(t)]
+        B = self.control_points[self.i_handle_before(t)]
+        C = self.control_points[self.i_handle_after(t)]
+        D = self.control_points[self.i_center_after(t)]
+        t = t % 1.0  # note, we have both `t` and `T` in this scope.
+        P = lerp(A, B, t)
+        Q = lerp(B, C, t)
+        R = lerp(C, D, t)
+        S = lerp(P, Q, t)
+        T = lerp(Q, R, t)  # `T` is a position, `t` is the interpolant
+        O = lerp(S, T, t)
+        # TODO: Reduce the number of `lerp`s by using Hermite polynomials
+        #       https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Representations
+        return O
+
+if __name__ == '__main__':
+    import visualization
+    visualization.main()
---
+++ visualization.py
@@ -0,0 +1,157 @@
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import itertools
+
+from cspline import CSpline, Vec2
+
+class DraggableNodes(pg.GraphItem):
+    def __init__(self, update_callback):
+        self.update_callback = update_callback
+        self.dragPoint = None
+        self.dragOffset = None
+        pg.GraphItem.__init__(self)
+
+    def setData(self, **kwds):
+        self.text = kwds.pop('text', [])
+        self.data = kwds
+        if 'pos' in self.data:
+            self.data['pos'] = np.array(self.data['pos'])
+            npts = self.data['pos'].shape[0]
+            self.data['data'] = np.empty(npts, dtype=[('index', int)])
+            self.data['data']['index'] = np.arange(npts)
+        self.updateGraph()
+
+    def updateGraph(self):
+        pg.GraphItem.setData(self, **self.data)
+        if 'pos' not in self.data:
+            return
+        control_points = self.data['pos']
+        self.update_callback(control_points)
+
+
+    def mouseDragEvent(self, ev):
+        if ev.button() != QtCore.Qt.LeftButton:
+            ev.ignore()
+            return
+
+        if ev.isStart():
+            # We are already one step into the drag.
+            # Find the point(s) at the mouse cursor when the button was first
+            # pressed:
+            pos = ev.buttonDownPos()
+            pts = self.scatter.pointsAt(pos)
+            if len(pts) == 0:
+                ev.ignore()
+                return
+            self.dragPoint = pts[0]
+            index = pts[0].data()[0]
+            self.dragOffset = self.data['pos'][index] - pos
+        elif ev.isFinish():
+            self.dragPoint = None
+            return
+        else:
+            if self.dragPoint is None:
+                ev.ignore()
+                return
+
+        index = self.dragPoint.data()[0]
+
+        dx = (ev.pos() + self.dragOffset) - self.data['pos'][index]
+        self.data['pos'][index] += dx
+        for i in children(index):
+            self.data['pos'][i] += dx
+
+        self.updateGraph()
+        ev.accept()
+
+# Some functions to calculate the control point nodes.
+def parent(i):
+    return (i//3) * 3
+def children(i):
+    if parent(i) != i:
+        return []
+    return [i+1, i+2]
+
+def main():
+
+    app = QtGui.QApplication([])
+    win = pg.GraphicsWindow(title="spline_regression")
+    win.resize(800,800)
+    pg.setConfigOptions(antialias=True)
+    view_box = win.addPlot()
+
+    # Define the spline control points
+    center_0 = Vec2(1, 2)
+    center_1 = Vec2(1, 4)
+    center_2 = Vec2(1, 8)
+    control_points = [
+        center_0,
+        center_0 - Vec2(1,1),
+        center_0 + Vec2(1,1),
+        center_1,
+        center_1 - Vec2(1,1),
+        center_1 + Vec2(1,1),
+        center_2,
+        center_2 - Vec2(1,2),
+        center_2 + Vec2(1,2),
+    ]
+
+    def make_control_point_line_data(control_points):
+        return np.array(list(itertools.chain(*(
+            [
+                control_points[i],
+                control_points[i+1],
+
+                control_points[i],
+                control_points[i+2],
+            ] for i in range(0, len(control_points), 3)
+        ))))
+    control_point_lines = pg.PlotDataItem(make_control_point_line_data(control_points), connect='pairs')
+
+    def make_curve_data(control_points):
+        """
+        Samples a spline defined by the control_points and turns it into
+        many small line segments.
+        Could optimize the number of lines by taking curvature and view_box ranges into account.
+        """
+        spline = CSpline(control_points)
+        t_extrapolation = 2.5  # how far to draw the straight lines past the
+        t_min = 0 - t_extrapolation
+        t_max = len(control_points)//3 - 1  + t_extrapolation
+        return np.array([
+            spline.get_pos(t)
+            for t in np.linspace(
+                t_min,
+                t_max,
+                num=50*(t_max-t_min) + 1
+            )
+        ])
+    spline_curve = pg.PlotDataItem(pen=pg.mkPen(width=2, color='#ff0000ee'))
+
+    def on_update(control_points):
+        control_point_lines.setData(make_control_point_line_data(control_points))
+        spline_curve.setData(make_curve_data(control_points))
+
+    draggables = DraggableNodes(on_update)
+    draggables.setData(
+        pos=control_points,
+        symbol=['o' if parent(i)==i else 's' for i in range(len(control_points)) ],
+        size=30,
+        symbolBrush=pg.mkBrush('#FFFFFF40')
+    )
+
+    view_box.addItem(draggables)
+    view_box.addItem(control_point_lines)
+    view_box.addItem(spline_curve)
+    # view_box.setAutoPan(x=False, y=False)
+    view_box.setRange(xRange=[-1,10], yRange=[-1,10])
+    view_box.showGrid(x=True, y=True)
+
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
+
+
+if __name__ == '__main__':
+    main()
