683c4809394057b5b06fbfad8521274c01586034
Dominik Schmid <dominikschmid93@gmail.com>
2020-09-15 11:43:42 +1000

    Split ML stuff into two files.

---
+++ ml_optimizer.py
@@ -0,0 +1,269 @@
+from gym import wrappers, logger, make as make_env
+from os import path
+from queue import Empty
+from typing import Optional, List, Tuple
+from cma import CMAEvolutionStrategy
+from cma.interfaces import OOOptimizer
+import argparse
+import json
+import sys
+import multiprocessing as mp
+import numpy as np
+import pickle
+
+from ml_policies import get_policy_class
+
+class ConstantEvolutionaryStrategy(OOOptimizer):
+    '''
+    This class just runs the given parameters again and again to give a distribution of outcomes.
+    '''
+    def __init__(self, parameters):
+        self.parameters = parameters
+        self.rewards = []
+
+    def ask(self, number=1000, **kwargs):
+        return [self.parameters] * number
+
+    def tell(self, _, rewards):
+        self.rewards += rewards
+
+    def disp(self):
+        mean = np.mean(self.rewards)
+        std = np.std(self.rewards)
+        print(f'{mean:3.2f} +-{std:3.2f}')
+
+    def stop(self):
+        return False
+
+class CrossEntopyMethodStrategy(OOOptimizer):
+    '''
+    Generic implementation of the cross-entropy method for minimizing a black-box function
+
+    Args:
+        parameters_mean (np.array): initial mean of distribution of parameters. Has a shape acceptible by f.
+        parameters_std (np.array): initial standard deviation of distribution of parameters. Same shape as parameters_mean.
+        elite_frac (float): each batch, select this fraction of the top-performing solutions
+    '''
+    def __init__(self, parameters_mean, parameters_std, elite_frac=.1):
+        self.parameters_mean = np.array(parameters_mean)
+        self.parameters_std = np.array(parameters_std)
+        self.elite_frac = elite_frac
+        assert len(self.parameters_mean.shape) == 1
+        assert self.parameters_mean.shape == self.parameters_std.shape
+
+    def ask(self, number=1000):
+        return np.array([
+            self.parameters_mean + dth
+            for dth in
+            self.parameters_std[None,:]*np.random.randn(number, self.parameters_mean.size)
+        ])
+
+    def tell(self, solutions, rewards):
+        assert len(solutions) == len(rewards)
+        n_elite = max(1, int(np.round(len(solutions)*self.elite_frac)))
+
+        # Keep the best performing parameters.
+        elite_indecies = np.argsort(rewards)[:n_elite]
+        elite_solutions = solutions[elite_indecies]
+        # Fit a gaussian distribution distribution to the elites.
+        self.parameters_mean = elite_solutions.mean(axis=0)
+        self.parameters_std = elite_solutions.std(axis=0)
+
+    def stop(self):
+        return self.parameters_std.mean() < 0.0001
+
+
+
+def do_rollout(agent, env, num_steps, render=False):
+    total_reward = 0
+    internal_repetitions = 10
+    for _ in range(internal_repetitions):
+        ob = env.reset()
+        for t in range(num_steps):
+            a = agent.act(ob)
+            (ob, reward, done, _info) = env.step(a)
+
+            total_reward += reward
+            if render and t%1==0: env.render()
+            if done: break
+    return total_reward / internal_repetitions
+
+def lowpriority():
+    """ Set the priority of the process to below-normal."""
+
+    import sys
+    try:
+        sys.getwindowsversion()
+    except AttributeError:
+        isWindows = False
+    else:
+        isWindows = True
+
+    if isWindows:
+        # Based on:
+        #   "Recipe 496767: Set Process Priority In Windows" on ActiveState
+        #   http://code.activestate.com/recipes/496767/
+        import win32api,win32process,win32con
+
+        pid = win32api.GetCurrentProcessId()
+        handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
+        win32process.SetPriorityClass(handle, win32process.BELOW_NORMAL_PRIORITY_CLASS)
+    else:
+        import os
+
+        os.nice(1)
+
+def evaluator_process(env_id: str, policy_id: str, seed: int, num_steps: int, render: bool,
+        parameters_q: mp.Queue, reward_q: mp.Queue):
+    '''
+    A class to hold data to run evaluations of the environment against a policy.
+    It is designed to be run.
+    parameters_q produces items like (ID: int, parameters: np.array)
+    reward_q receives items like (ID: int, reward: float)
+    '''
+    env = make_env(env_id)
+    env.seed(seed)
+    np.random.seed(seed)
+
+    policy_class = get_policy_class(policy_id)
+    lowpriority()
+    try:
+        while True:
+            (i, parameters) = parameters_q.get(block=True)
+            agent = policy_class(parameters)
+            reward = do_rollout(agent, env, num_steps, render)
+            reward_q.put((i, reward), block=True)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    logger.set_level(logger.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--display', action='store_true')
+    parser.add_argument('env_id', nargs='?', default='Pendulum-v0')
+    parser.add_argument('policy_id', nargs='?', default='EnergyPendulumPolicy')
+    args = parser.parse_args()
+
+    # Generate our parameter list by running once.
+    render_env = make_env(args.env_id)
+    bootstrap = get_policy_class(args.policy_id)(parameters=None)
+    bootstrap.act(render_env.reset())
+
+    # ----------------------------------------
+    # You provide the directory to write to (can be an existing
+    # directory, but can't contain previous monitor results. You can
+    # also dump to a tempdir if you'd like: tempfile.mkdtemp().
+    outdir = '/tmp/cem-agent-results'
+    # Prepare snapshotting
+    def writefile(fname, s):
+        with open(path.join(outdir, fname), 'w') as fh: fh.write(s)
+    # Write out the env so we store the parameters of this environment.
+    writefile('info.json', json.dumps({
+        'argv': sys.argv,
+        'env_id': args.env_id,
+        'policy_id': args.policy_id,
+    }))
+    # ------------------------------------------
+
+
+    # Set up paralellism for running evaluations (multiprocessing gets around Python's GIL)
+    num_steps = num_steps = 200
+    num_processes = 10
+    eval_timeout = 1.0  # maximum seconds for a single result to come back
+    ctx = mp.get_context('spawn')
+    parameters_q = ctx.Queue()
+    reward_q = ctx.Queue()
+    evaluators = [
+        ctx.Process(
+            target=evaluator_process,
+            args=(args.env_id, args.policy_id, i, num_steps, False, parameters_q, reward_q),
+            daemon=True
+        )
+        for i in range(num_processes)
+    ]
+    for evaluator in evaluators:
+        evaluator.start()
+
+    # Allow rendering of the current batch while we train the next.
+    render_timeout = 20.0
+    render_parameters_q = ctx.Queue()
+    render_reward_q = ctx.Queue()
+    render_evaluator = ctx.Process(
+        target=evaluator_process,
+        args=(args.env_id, args.policy_id, 0, num_steps, True, render_parameters_q, render_reward_q),
+        daemon=True
+    )
+    render_evaluator.start()
+
+
+    def evaluate_batch(solutions: List[np.array]) -> List[float]:
+        for i, parameters in enumerate(solutions):
+            parameters_q.put((i, list(parameters)))
+
+        results = [None] * len(solutions)
+        for _ in range(len(solutions)):
+            try:
+                (i, result) = reward_q.get(block=True, timeout=eval_timeout)
+                results[i] = result
+            except Empty:
+                raise RuntimeError('Not all evaluations completed. :(')
+        assert all(result is not None for result in results)
+        return results
+
+    # Define our optimizer.
+    # es = CMAEvolutionStrategy(
+    #     bootstrap.parameters,
+    #     np.array(bootstrap.parameters_std).mean(),
+    #     {}
+    # )
+    es = CrossEntopyMethodStrategy(
+        bootstrap.parameters,
+        bootstrap.parameters_std,
+        elite_frac=0.01,
+    )
+    fixed_parameters = None
+    if fixed_parameters is not None:
+        es = ConstantEvolutionaryStrategy(fixed_parameters)
+
+
+    # Train the agent, and snapshot each stage.
+    iterdata = None
+    batch_best = None
+    try:
+
+        i = 0
+        while not es.stop():
+            solutions = es.ask(number=1000)
+            rewards = evaluate_batch(solutions)
+            batch_best = solutions[np.argmax(rewards)]
+            es.tell(solutions, [-reward for reward in rewards])  # negate because es minimizes.
+            # es.disp()
+            print(f'i={i:4d} {es.__class__.__name__} {args.policy_id} mean reward: {np.mean(rewards) :4.1f}')
+            # Do a little async preview of the current best estimate.
+            if args.display:
+                if render_parameters_q.empty():
+                    render_parameters_q.put((i, batch_best))
+                if not render_reward_q.empty():
+                    _ = render_reward_q.get(block=True, timeout=render_timeout)
+            i += 1
+
+    except KeyboardInterrupt:
+        print('cancelled')
+    finally:
+        if batch_best is not None:
+            print('current batch_best: ', list(batch_best))
+
+        # try to work around https://bugs.python.org/issue41761
+        for p in evaluators:
+            p.kill()
+            p.join(timeout=.1)
+            p.close()
+        parameters_q.close()
+        reward_q.close()
+
+        render_evaluator.kill()
+        render_evaluator.join(timeout=.1)
+        render_evaluator.close()
+        render_parameters_q.close()
+        render_reward_q.close()
---
+++ ml_policies.py
@@ -0,0 +1,187 @@
+import numpy as np
+from typing import Optional, List, Tuple
+
+class ParameterCreatingPolicy():
+    '''
+    This class can create or read parameters when act() executes.
+    This allows fast iteration on the act() code without needing to change
+    the declarations of parameters since this declaration information
+    is created in a dummy run of the act() function.
+    '''
+    MODE_CREATE_PARAMS = 1
+    MODE_READ_PARAMS = 2
+
+    def __init__(self, parameters: Optional[np.array]=None):
+        '''
+        When parameters are not specified, the default values are chosen in when act()
+        \which then become visible.
+        When parameters are specified, it is assumed they match the shape of the default parameters.
+        '''
+        self.mode = self.MODE_CREATE_PARAMS if parameters is None else self.MODE_READ_PARAMS
+        self.parameters = [] if parameters is None else parameters  # these are our parameters to be tuned by the optimizer
+        self.parameters_std = [] if parameters is None else None   # some hints for the optimizer for their standard deviations.
+        self.current_parameter = 0
+
+    def param(self, guess_value: float, guess_std: Optional[float]=None):
+        '''
+        In create mode:
+            Create parameters with an estimation over their initial value.
+            guess_value is returned.
+        In read mode:
+            return a value from parameters as passed to __init__()
+            by using a counter which assumes we get called in the same order.
+            Arguments to this function are discarded.
+        '''
+        if self.mode == self.MODE_CREATE_PARAMS:
+            self.parameters.append(guess_value)
+            if guess_std is None:
+                if guess_value == 0:
+                    guess_std = 1
+                else:
+                    guess_std = abs(guess_value)
+            self.parameters_std.append(guess_std)
+            return guess_value
+        elif self.mode == self.MODE_READ_PARAMS:
+            assert 0 <= self.current_parameter < len(self.parameters), f'make sure to reset self.current_parameter and to always call self.param() the same amount of times. \n got current_parameter={self.current_parameter}, want 0 <= current_parameter < {len(self.parameters)}'
+            parameter = self.parameters[self.current_parameter]
+            self.current_parameter += 1
+            return parameter
+
+    def act(self, observation):
+        self.current_parameter = 0  # any subclass must reset this counter!
+        raise NotImplementedError()
+
+class EnergyPendulumPolicy(ParameterCreatingPolicy):
+    '''
+    A policy to solve Pendulum-v0
+    '''
+    def act(self, observation):
+        self.current_parameter = 0
+        p = self.param  # shorthand for creating or reading parameters.
+
+        y, x, thetadot = observation
+
+        torque_add_energy = thetadot*10000
+        torque_top_control = p(0) * x + p(0) * thetadot + p(0) * y + p(0) * x**2
+        energy = y * p(1) + abs(thetadot)
+        torque = torque_add_energy if energy < p(1, 10) else torque_top_control
+
+        return [torque]
+
+class PiecewisePendulumPolicy(ParameterCreatingPolicy):
+    '''
+    A piecewise linear policy to solve Pendulum-v0
+    '''
+    def act(self, observation):
+        self.current_parameter = 0
+        p = self.param  # shorthand for creating or reading parameters.
+
+        squared = observation * observation # square each variable
+        all_variables = np.hstack([observation, squared])
+
+        def p_vec3():
+            return np.array([p(0), p(0), p(0)])
+
+        def linear_function():
+            return p_vec3().dot(observation)
+
+        num_piecewise = 3
+        conditions = [linear_function() > 0 for _ in range(num_piecewise)]
+        torques = [linear_function() for _ in range(num_piecewise)]
+        for condition, torque in zip(conditions, torques):
+            if condition:
+                return [torque]
+
+        return [torques[-1]]
+
+class NeuralNetPendulumPolicy(ParameterCreatingPolicy):
+    '''
+    A policy that uses a neural net to make decisions
+    '''
+    def act(self, observation):
+        self.current_parameter = 0
+        p = self.param  # shorthand for creating or reading parameters.
+
+        layers = [
+            np.array(observation),
+            np.zeros(shape=(3,)),
+            np.zeros(shape=(3,)),
+            np.zeros(shape=(1,)),
+        ]
+        connections = []
+        for i in range(len(layers)-1):
+            matrix = np.array([
+                [p(0) for _ in range(len(layers[i]))]
+                for _ in range(len(layers[i+1]))
+            ])
+            bias = [p(0) for _ in range(len(layers[i+1]))]
+            layers[i+1] = np.tanh(np.dot(matrix, layers[i]) + bias)
+
+        return layers[-1]
+        # should_negate = -1 if layers[-1][1] > 0 else 1
+        # return [layers[-1][0] * should_negate * 2.1]  # usually output something in the range -2..2
+
+
+class CartpolePolicy(ParameterCreatingPolicy):
+    '''
+    A policy to solve CartPole-v0
+    '''
+    def act(self, observation):
+        self.current_parameter = 0
+        p = self.param  # shorthand for creating or reading parameters.
+
+        x, x_dot, theta, theta_dot = observation
+
+        go_right = (
+            p(0) * x +
+            p(0) * x_dot +
+            p(0) * theta +
+            p(0) * theta_dot
+        ) > 0
+        return 1 if go_right else 0
+
+class CartpolePIDPolicy(ParameterCreatingPolicy):
+    '''
+    A PID controller for CartPole-v0 based on
+    https://gist.github.com/HenryJia/23db12d61546054aa43f8dc587d9dc2c
+    without knowing the PID values.
+    '''
+
+    desired_state = np.array([0, 0, 0, 0])
+    desired_mask  = np.array([0, 0, 1, 0])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.integral = 0
+        self.derivative = 0
+        self.prev_error = 0
+
+    def act(self, state):
+        self.current_parameter = 0
+        p = self.param  # shorthand for creating or reading parameters.
+
+        error = state - self.desired_state
+        self.integral += error
+        self.derivative = error - self.prev_error
+        self.prev_error = error
+
+        P, I, D = p(0.0), p(0.0), p(0.0)
+        # P, I, D = p(.1, .01), p(.01, .01), p(.5, .01)  # uncommenting this would be cheating.
+        pid = np.dot(P * error + I * self.integral + D * self.derivative, self.desired_mask)
+        def sigmoid(x):
+            return 1.0 / (1.0 + np.exp(-x))
+        action = sigmoid(pid)
+        action = np.round(action).astype(np.int32)
+        return action
+
+def get_policy_class(policy_id):
+    policy_id_to_policy_class = {
+        'EnergyPendulumPolicy': EnergyPendulumPolicy,
+        'PiecewisePendulumPolicy': PiecewisePendulumPolicy,
+        'NeuralNetPendulumPolicy': NeuralNetPendulumPolicy,
+        'CartpolePolicy': CartpolePolicy,
+        'CartpolePIDPolicy': CartpolePIDPolicy,
+    }
+    if policy_id not in policy_id_to_policy_class:
+        raise NotImplementedError(f'No policy_id not found: {repr(policy_id)}')
+    return policy_id_to_policy_class[policy_id]
